b6dd649f38c0c144eed62d119748e2407723416d
akashbaidya015 <71338450+akashbaidya015@users.noreply.github.com>
2024-12-05 12:10:56 -0500

    Update layout.tsx

--- bda_web_git/app/layout.tsx
+++ bda_web_git/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <head>
         {/* Add the Google Font link here */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat+Subrayada:wght@400;700&display=swap"
           rel="stylesheet"
